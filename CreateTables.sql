817aa61d073c874b0d3e8be1a82e7275bc9313ec
MikaAkk <miagzgx@gmail.com>
2024-10-04 20:53:57 +0300

    Add files via upload

---
+++ CreateTables.sql
@@ -0,0 +1,150 @@
+﻿/*
+ * База данных «Прокат автомобилей»
+ *
+ * База данных должна включать как минимум таблицы КЛИЕНТЫ, АВТОМОБИЛИ, 
+ * ФАКТЫ_ПРОКАТА, содержащие следующую информацию:
+ *     • Фамилия клиента
+ *     • Имя клиента
+ *     • Отчество клиента
+ *     • Серия, и номер паспорта клиента
+ *     • Адрес проживания клиента: улица, дом, квартира
+ *     • Производитель (бренд) автомобиля
+ *     • Модель автомобиля
+ *     • Цвет автомобиля 
+ *     • Год выпуска автомобиля
+ *     • Госномер автомобиля
+ *     • Страховая стоимость автомобиля
+ *     • Стоимость одного дня проката
+ *     • Дата начала проката
+ *     • Количество дней проката
+ *
+ */
+
+print(N'*** Старт скрипта создание таблиц базы данных ***' + char(13));
+
+-- при повторном запуске скрипта удаляем старые варианты таблиц, не разбирая пустые они или нет
+-- таблицы удаляем в порядке, обратном порядку создания
+print(N'    Удаление предыдущих версий таблиц базы данных');
+drop table if exists Hires;   -- факты проката
+drop table if exists Clients; -- клиенты
+drop table if exists Streets; -- улицы
+drop table if exists Cars;    -- автомобили в арендной фирмы
+drop table if exists Colors;  -- цвета автомобилей
+drop table if exists Brands;  -- название бренда
+drop table if exists Models;  -- название модели
+print('    OK' + char(13));
+
+-- названия брендов автомобилей
+print(N'    Создание таблицы справочника ПРОИЗВОДИТЕЛЕЙ');
+create table Brands (
+	Id      int          not null primary key identity (1, 1),
+	[Name]  nvarchar(30) not null    -- название бренда
+);
+print('    OK' + char(13));
+go
+
+
+-- названия моделей автомобилей
+print(N'    Создание таблицы справочника МОДЕЛЕЙ');
+create table Models (
+	Id      int          not null primary key identity (1, 1),
+	[Name]  nvarchar(30) not null    -- название модели
+);
+print('    OK' + char(13));
+go
+
+
+-- названия цветов автомобилей
+print(N'    Создание таблицы справочника ЦВЕТА АВТОМОБИЛЕЙ');
+create table Colors (
+	Id      int          not null primary key identity (1, 1),
+	[Name]  nvarchar(30) not null    -- название цвета
+);
+go
+print('    OK' + char(13));
+go
+
+-- названия улиц
+print(N'    Создание таблицы справочника НАЗВАНИЯ УЛИЦ');
+create table Streets (
+	Id          int          not null primary key identity (1, 1),
+	[Name]      nvarchar(30) not null    -- название улицы
+);
+print('    OK' + char(13));
+go
+
+
+-- автомобили фирмы проката
+print(N'    Создание таблицы справочника АВТОМОБИЛИ');
+-- таблица 
+create table dbo.Cars (
+    Id          int          not null primary key identity (1, 1),
+	IdBrand     int          not null, -- внешний ключ, ссылка на Bravds, бренд автомобиля
+	IdModel     int          not null, -- внешний ключ, ссылка на Models, модель автомобиля
+	IdColor     int          not null, -- внешний ключ, ссылка на Colors, цвет автомобиля
+	Plate       nvarchar(12) not null, -- гос. регистрационный номер автомобиля
+	YearManuf   int          not null, -- год производства автомобиля
+	InsurValue  int          not null, -- страховая стоимость
+	Rental      int          not null, -- стоимость одного дня проката
+
+	-- проверочные ограничения
+	constraint  CK_Cars_YearManuf   check(YearManuf >= 2010),
+	constraint  CK_Cars_InsurValue  check(InsurValue > 0),
+
+	-- ограничение уникальности значения столбца
+	-- https://docs.microsoft.com/ru-ru/sql/relational-databases/tables/create-unique-constraints?view=sql-server-ver15
+	constraint  CK_Cars_Plate       unique(Plate),
+	
+	constraint  CK_Cars_Rental      check(Rental > 0),
+
+	-- внешние ключи
+	constraint  FK_Cars_Brands foreign key (IdBrand) references dbo.Brands(Id), 
+	constraint  FK_Cars_Models foreign key (IdModel) references dbo.Models(Id), 
+	constraint  FK_Cars_Colors foreign key (IdColor) references dbo.Colors(Id) 
+);
+print('    OK' + char(13));
+go
+
+-- клиенты фирмы проката
+print(N'    Создание таблицы справочника КЛИЕНТЫ');
+create table dbo.Clients (
+	Id          int          not null primary key identity (1, 1),
+	Surname     nvarchar(60) not null,    -- Фамилия клиента
+	[Name]      nvarchar(50) not null,    -- Имя клиента
+	Patronymic  nvarchar(60) not null,    -- Отчество клиента
+	Passport    nvarchar(15) not null,    -- Серия и номер паспорта
+
+	IdStreet    int          not null,    -- внешний ключ, ссылка на Streets, название улицы
+	Building    nvarchar(12) not null,    -- номер дома, может включать буквы, 12а или 8-бис
+	Flat        int,                      -- номер квартиры, не обязателен, для частного сектора это нормально
+
+	-- ограничение уникальности значения столбца
+	constraint  CK_Clients_Passport unique(Passport),
+
+	-- внешний ключ
+	constraint  FK_Clients_Streets foreign key (IdStreet) references dbo.Streets(Id)
+);
+print('    OK' + char(13));
+go
+
+-- факты проката
+print(N'    Создание таблицы регистра ФАКТЫ_ПРОКАТА');
+create table Hires(
+	Id          int   not null primary key identity (1, 1),
+	IdClient    int   not null,  -- внешний ключ, ссылка на таблицу Clients 
+	IdCar       int   not null,  -- внешний ключ, ссылка на таблицу Cars
+	DateStart   date  not null,  -- дата начала проката
+	Duration    int   not null,  -- длительность проката
+
+	-- проверочные ограничения
+	constraint  CK_Hires_DateStart check(DateStart > '01-01-2023'),
+	constraint  CK_Hires_Duration  check(Duration between 1 and 15),
+
+	-- внешние ключи
+	constraint  FK_Hires_Clients   foreign key (IdClient) references dbo.Clients(Id),
+	constraint  FK_Hires_Cars      foreign key (IdCar)    references dbo.Cars(Id)
+);
+print('    OK' + char(13));
+go
+
+print(N'*** Финиш скрипта ***');
---
+++ Queries.sql
@@ -0,0 +1,432 @@
+﻿/* База данных «Прокат автомобилей» */
+
+-- вывод вспомогательных справочников
+select * from Streets;
+select * from Brands;
+select * from Models;
+select * from Colors;
+go
+
+-- Вывод записей таблицы автомобилей с расшифровкой всех полей
+select
+    Cars.Id 
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+    , Cars.Plate
+    , Cars.YearManuf
+    , Cars.InsurValue
+    , Cars.Rental
+from
+    Cars join Brands on Cars.IdBrand = Brands.Id
+         join Models on Cars.IdModel = Models.Id
+         join Colors on Cars.IdColor = Colors.Id;
+go
+
+-- Вывод таблицы КЛИЕНТЫ с расшифровкой всех полей
+select
+    Clients.Id
+    , Clients.Surname
+    , Clients.[Name]
+    , Clients.Patronymic
+    , Clients.Passport
+    , Streets.[Name]      as Street
+    , Clients.Building
+    , Clients.Flat
+from
+    Clients join Streets on Clients.IdStreet = Streets.Id;
+go
+
+
+-- Вывод записей таблицы фактов проката с расшифровкой всех полей
+select
+    Hires.Id
+    , Hires.DateStart
+    , Hires.Duration
+
+    , Cars.Plate
+    , Cars.Rental
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+
+    , Clients.Surname + ' ' + Substring(Clients.[Name], 1, 1) + '.' + 
+          Substring(Clients.Patronymic, 1, 1) + '.' as Client
+    , Clients.Passport
+    , Streets.[Name] as Street
+    , Clients.Building
+    , Clients.Flat
+from
+    Hires join (Cars join Brands on Cars.IdBrand = Brands.Id
+                     join Models on Cars.IdModel = Models.Id
+                     join Colors on Cars.IdColor = Colors.Id) 
+               on Hires.IdCar = Cars.Id
+          join (Clients join Streets on Clients.IdStreet = Streets.Id) on Hires.IdClient = Clients.Id
+-- order by
+--    Cars.Plate
+;
+go
+
+--  1. Запрос с параметром	
+--     Выбирает из таблицы АВТОМОБИЛИ информацию об автомобилях заданной модели
+--     (например, ВАЗ-2110)
+declare @brand nvarchar(30) = 'Skoda', @model nvarchar(30) = 'Octavia A5';
+-- declare @brand nvarchar(30) = 'Suzuki', @model nvarchar(30) = 'Grand Vitara';
+
+select
+    Cars.Id 
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+    , Cars.Plate
+    , Cars.YearManuf
+    , Cars.InsurValue
+    , Cars.Rental
+from
+    Cars join Brands on Cars.IdBrand = Brands.Id
+         join Models on Cars.IdModel = Models.Id
+         join Colors on Cars.IdColor = Colors.Id
+where
+    Brands.[Name] = @brand and Models.[Name] = @model;
+go
+
+-- вывести факты проката автомобиля с заданным номерным знаком
+declare @plate nvarchar(15) = N'Н179РК';
+select
+    Hires.Id
+    , Hires.DateStart
+    , Hires.Duration
+
+    , Cars.Plate
+    , Cars.Rental
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+
+    , Clients.Surname + ' ' + Substring(Clients.[Name], 1, 1) + '.' + 
+          Substring(Clients.Patronymic, 1, 1) + '.' as Client
+    , Clients.Passport
+    , Streets.[Name] as Street
+    , Clients.Building
+    , Clients.Flat
+from
+    Hires join (Cars join Brands on Cars.IdBrand = Brands.Id
+                     join Models on Cars.IdModel = Models.Id
+                     join Colors on Cars.IdColor = Colors.Id) 
+               on Hires.IdCar = Cars.Id
+          join (Clients join Streets on Clients.IdStreet = Streets.Id) on Hires.IdClient = Clients.Id
+where
+     Cars.Plate = @plate;
+go
+
+
+--  2. Запрос с параметром	
+--     Выбирает из таблицы АВТОМОБИЛИ информацию об автомобилях, изготовленных 
+--     до заданного года (например, до 2016)
+declare @yearManuf int = 2018;
+
+select
+    Cars.Id 
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+    , Cars.Plate
+    , Cars.YearManuf
+    , Cars.InsurValue
+    , Cars.Rental
+from
+    Cars join Brands on Cars.IdBrand = Brands.Id
+         join Models on Cars.IdModel = Models.Id
+         join Colors on Cars.IdColor = Colors.Id
+where
+    Cars.YearManuf < @yearManuf;
+go
+
+--  3. Запрос с параметром	
+--     Выбирает из таблицы АВТОМОБИЛИ информацию об автомобилях, имеющих 
+--     заданные модель и цвет, изготовленных после заданного года
+declare @brand nvarchar(30) = 'Suzuki', @model nvarchar(30) = 'Grand Vitara', @color nvarchar(30)=N'серебристый', 
+        @yearManuf int = 2016;
+
+select
+    Cars.Id 
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+    , Cars.Plate
+    , Cars.YearManuf
+    , Cars.InsurValue
+    , Cars.Rental
+from
+    Cars join Brands on Cars.IdBrand = Brands.Id
+         join Models on Cars.IdModel = Models.Id
+         join Colors on Cars.IdColor = Colors.Id
+where
+    Brands.[Name] = @brand and Models.[Name] = @model and Colors.[Name] = @color and Cars.YearManuf > @yearManuf;
+go
+
+--  4. Запрос с параметром	
+--     Выбирает из таблицы АВТОМОБИЛИ информацию об автомобиле с заданным
+--     госномером
+-- declare @plate nvarchar(12) = N'В015РК';
+declare @plate nvarchar(12) = N'С';
+
+select
+    Cars.Id 
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+    , Cars.Plate
+    , Cars.YearManuf
+    , Cars.InsurValue
+    , Cars.Rental
+from
+    Cars join Brands on Cars.IdBrand = Brands.Id
+         join Models on Cars.IdModel = Models.Id
+         join Colors on Cars.IdColor = Colors.Id
+where
+    -- такой вариант лучше при поиске конкретного номера 
+    -- Cars.Plate = @plate;
+   Cars.Plate like @plate + '%';
+go
+
+
+--  5. Запрос с параметром	
+--     Выбирает из таблиц КЛИЕНТЫ, АВТОМОБИЛИ и ФАКТЫ_ПРОКАТА информацию обо
+--     всех зафиксированных фактах проката автомобилей (ФИО клиента, Модель 
+--     автомобиля, Госномер автомобиля, дата проката) в некоторый заданный 
+--     интервал времени. Нижняя и верхняя границы интервала задаются при 
+--     выполнении запроса
+--     формат даты:   мм-дд-гггг
+declare @from date = '03-01-2023', @to date = '03-31-2023';
+
+select
+    Hires.Id
+    , Clients.Surname + ' ' + Substring(Clients.[Name], 1, 1) + '.' + 
+          Substring(Clients.Patronymic, 1, 1) + '.' as Client
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Cars.Plate
+    , Hires.DateStart
+    , Hires.Duration
+from
+    Hires join (Cars join Brands on Cars.IdBrand = Brands.Id
+                     join Models on Cars.IdModel = Models.Id) 
+               on Hires.IdCar = Cars.Id
+          join Clients on Hires.IdClient = Clients.Id
+where
+    Hires.DateStart between @from and @to 
+order by
+    Hires.DateStart;
+go
+
+--  6. Запрос с вычисляемыми полями	
+--     Вычисляет для каждого факта проката стоимость проката. Включает поля 
+--     Дата проката, Госномер автомобиля, Модель автомобиля, Стоимость проката.
+--     Сортировка по полю Дата проката
+select
+    Hires.Id
+    , Hires.DateStart
+    , Cars.Plate
+    , Brands.[Name]                 as Brand
+    , Models.[Name]                 as Model
+    , Colors.[Name]                 as Color
+    , Cars.Rental                   as RentalPerDay
+    , Hires.Duration
+    , Cars.Rental * Hires.Duration as Price 
+from
+    Hires join (Cars join Brands on Cars.IdBrand = Brands.Id
+                     join Models on Cars.IdModel = Models.Id
+                     join Colors on Cars.IdColor = Colors.Id) 
+               on Hires.IdCar = Cars.Id
+order by
+    Hires.DateStart;
+go
+
+
+-- 7. Запрос с подзапросом	
+--    Вывести автомобили прокатной фирмы с годом выпуска, меньшим года выпуска
+--    всех автомобилей заданного бренда, использовать all
+declare @brand nvarchar(30)='Suzuki';
+
+select
+    Cars.Id 
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Colors.[Name] as Color
+    , Cars.Plate
+    , Cars.YearManuf
+    , Cars.InsurValue
+    , Cars.Rental
+from
+    Cars join Brands on Cars.IdBrand = Brands.Id
+         join Models on Cars.IdModel = Models.Id
+         join Colors on Cars.IdColor = Colors.Id
+where
+    Cars.YearManuf < all (select Cars.YearManuf 
+                          from Cars join Brands on Cars.IdBrand = Brands.Id 
+                          where Brands.[Name] = @brand /* 'Suzuki' */ )
+go
+
+-- 8. Запрос с подзапросом	
+--    Вывести факты проката с количеством дней проката большим максимального 
+--    количества дней проката автомобиля заданного бренда, использовать some 
+--    (any)
+declare @brand nvarchar(30)='Skoda';
+
+select
+    Hires.Id
+    , Clients.Surname + ' ' + Substring(Clients.[Name], 1, 1) + '.' + 
+          Substring(Clients.Patronymic, 1, 1) + '.' as Client
+    , Brands.[Name] as Brand
+    , Models.[Name] as Model
+    , Cars.Plate
+    , Hires.DateStart
+    , Hires.Duration
+from
+    Hires join (Cars join Brands on Cars.IdBrand = Brands.Id
+                     join Models on Cars.IdModel = Models.Id) 
+               on Hires.IdCar = Cars.Id
+          join Clients on Hires.IdClient = Clients.Id
+where 
+    -- используем any/some по заданию
+    Hires.Duration > any(select Hires.Duration
+                        from   Hires join (Cars join Brands on Cars.IdBrand = Brands.Id) on Hires.IdCar = Cars.Id
+                        where  Brands.[Name] = @brand    /* 'Skoda' */)
+    
+    -- эквивалент без any/some
+   -- Hires.Duration > (select Min(Hires.Duration) 
+   --                   from   Hires join (Cars join Brands on Cars.IdBrand = Brands.Id) on Hires.IdCar = Cars.Id
+   --                   where  Brands.[Name] = @brand  /* 'Skoda'  */)
+order by
+    Hires.DateStart;
+go
+
+
+--  9. Запрос с левым соединением	
+--     Для всех автомобилей прокатной фирмы вычисляет количество фактов 
+--     проката, сумму вырученную за прокаты
+select
+    Cars.Id
+    , Cars.Plate
+    , Brands.[Name]      as Brand
+    , Models.[Name]      as Model
+    , Count(Hires.IdCar) as Amount
+    , IsNull(Sum(Cars.Rental * Hires.Duration), 0) as SumRental
+from
+    (Cars join Brands on Cars.IdBrand = Brands.Id join Models on Cars.IdModel = Models.Id)
+    left join 
+    Hires on Cars.Id = Hires.IdCar
+group by
+    Cars.Id, Cars.Plate, Brands.[Name], Models.[Name] 
+order by
+    SumRental desc;
+go
+
+
+-- 10. Итоговый запрос	
+--     Выполняет группировку по полю Год выпуска автомобиля. Для каждого года 
+--     вычисляет минимальное и максимальное значения по полю Стоимость одного 
+--     дня проката
+select
+    YearManuf
+    , Count(*)    as AmountYearManuf
+    , Min(Rental) as MinRental
+    , Max(Rental) as MaxRental
+from  
+    Cars
+group by
+    YearManuf;
+go
+
+
+-- 11. Запрос на добавление	
+--     Добавляет в таблицу ФАКТЫ_ПРОКАТА данные о факте проката. 
+--     Данные передавайте параметрами, используйте подзапросы
+declare @passport nvarchar(15) = '13 21 185659', @plate nvarchar(12) = N'С192РК', 
+        @dateStart date = '05-26-2023', @duration int = 12;
+
+insert Hires
+    (IdClient, IdCar, DateStart, Duration)
+values (
+    (select Id from Clients where Passport = @passport),
+    (select Id from Cars where Plate = @plate), 
+    @dateStart, @duration);
+go
+
+-- 12. Запрос на добавление	
+--     Добавляет в таблицу АВТОМОБИЛИ данные о новом автомобиле в прокатной 
+--     фирме. Данные автомобиля задавайте параметрами, используйте подзапросы
+declare 
+    @brand nvarchar(30)='Mercedes', @model nvarchar(3)='210', @color nvarchar(30) = N'серебристый',
+    @plate nvarchar(12)=N'М345РК', @yearManuf int = 2020, @insurValue int = 4500000,
+    @rental int = 5100;
+
+insert Cars
+   (IdBrand, IdModel, IdColor, Plate, YearManuf, InsurValue, Rental)
+values (
+   (select Id from Brands where [Name] = @brand),
+   (select Id from Models where [Name] = @model),
+   (select Id from Colors where [Name] = @color),
+   @plate, @yearManuf, @insurValue, @rental);
+go
+
+
+-- 13. Запрос на удаление	
+--     Удаляет из таблицы ФАКТЫ_ПРОКАТА запись по идентификатору, заданному 
+--     параметром запроса
+declare @id int = 21;
+
+delete from  
+    Hires
+where
+    Id = @id;
+go
+
+
+-- 14. Запрос на удаление	
+--     Удаляет из таблицы ФАКТЫ_ПРОКАТА записи за указанный период 
+--     для заданного клиента
+declare @passport nvarchar(15) = N'12 21 123122', @from date = '05-01-2023', @to date = '05-31-2023';
+
+delete from  
+    Hires
+where
+    IdClient = (select Id from Clients where Passport = @passport)
+    and 
+    DateStart between @from and @to;
+go
+
+
+-- 15. Запрос на обновление	
+--     Увеличивает значение в поле Стоимость одного дня проката на заданное 
+--     количество процентов для автомобилей, изготовленных после заданного года
+declare @percent float = 10, @yearManuf int = 2018;
+
+update
+    Cars
+set
+    Rental *= (100 + @percent) / 100
+where
+    Cars.YearManuf > @yearManuf;
+go
+
+-- 16. Запрос на обновление	
+--     Изменяет данные клиента по его идентификатору на указанные в параметрах 
+--     запроса значение
+declare @id int = 1,
+	@surname    nvarchar(60) = N'Иванов',
+	@name       nvarchar(50) = N'Иван',
+	@patronymic nvarchar(60) = N'Иванович',
+    @passport   nvarchar(15) = N'99 21 000001';
+
+update
+    Clients
+set
+    Surname = @surname
+    , [Name] = @name
+    , Patronymic = @patronymic
+    , Passport = @passport
+where
+    Clients.Id = @id;
+go
